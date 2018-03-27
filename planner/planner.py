50f3b1b9cec59a058c8889e16bc4012c66fe25a4
BadassBanana <alejandrohurtado888@gmail.com>
2018-03-27 12:27:04 +0100

    Cleaned a few things up

--- planner/planner.py
+++ planner/planner.py
@@ -1,6 +1,6 @@
 # Created by Alex Hurtado
-
 import sqlite3
+import click
 
 
 class Planner(object):
@@ -9,9 +9,9 @@ class Planner(object):
         self.db = db
         self.connected = None
 
-    def open_connection(self, db):
+    def open_connection(self):
         """Opens a database connection."""
-        self.conn = sqlite3.connect(db)
+        self.conn = sqlite3.connect("planner.db")
         self.connected = True
         self.cur = self.conn.cursor()
 
@@ -30,13 +30,12 @@ class Planner(object):
         else:
             return False
 
-    def make_db(self, db):
+    def make_db(self):
         """Makes a database, if it doesn't exist already"""
         self.open_connection()
         self.cur.execute('''CREATE TABLE events
                               (date text, events_name text)''')
         self.conn.commit()
-        self.close_connection()
 
     def create_event(self, date, event):
         """Creates an event inside of a database"""
@@ -46,3 +45,11 @@ class Planner(object):
         self.cur.execute('INSERT INTO events (date, events_name) VALUES (?, ?)', (event, date))
         self.conn.commit()
         self.close_connection()
+        
+    def read_all(self):
+        """Reads the whole database and prints it out"""
+        print("Collecting data from database....")
+        self.info = self.cur.execute('''SELECT * FROM events''').fetchall()
+        print("Here are the events that you have set: ")
+        for i in self.info:
+            print(i)
