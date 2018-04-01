0cee377ecd96317ff473fda28a3ff286ec45be5a
BadassBanana <alejandrohurtado888@gmail.com>
2018-04-01 18:07:29 +0100

    Forgot to clean out a few things

--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
-from planner.__init__ import __version__
+from termplanner.__init__ import __version__
 from setuptools import setup, find_packages
 
-setup(name='planner',
+setup(name='termplanner',
       version= __version__,
-      py_modules=['planner'],
+      py_modules=['termplanner'],
       install_requires=[
           'Click',
           ],
       entry_points='''
           [console_scripts]
-          planner=planner.main:cli
+          planner=termplanner.main:cli
       ''', # This is actually referring to a cli() function...
-      description='Because you want to plan your day on the command line',
+      description='A simple command line application to plan your day.',
       url='http://github.com/ModoUnreal/Planner',
       author='Alex Hurtado',
       author_email='modounreal@gmail.com',
       license='MIT',
       packages=find_packages(),
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=(
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'License :: OSI Approved :: MIT License',
 
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3'
+          ),
+)
