e877668f3cca8b1cf7b1f5049b20235d10f55fb1
BadassBanana <alejandrohurtado888@gmail.com>
2018-04-10 17:32:13 +0100

    Fixed up dates module

--- termplanner/dates.py
+++ termplanner/dates.py
@@ -9,6 +9,9 @@ def find_years(date):
     if date[date_length - 4:].find('.') == -1:
         year = date[date_length - 4:]
 
+    elif date[date_length - 4].find('.') != -1 and date_length != 10:
+        year = date[:date_length - 7]
+
     else:
         year = date[:date_length - 6]
 
@@ -17,8 +20,11 @@ def find_years(date):
 def find_days(date):
     """Used to find the days in a date string."""
     date_length = len(date)
-    if date[date_length - 3:].find('.') != 0:
-        day = date[:date_length - 8]
+    if date[date_length - 3:].find('.') == -1:
+        if date_length == 8:
+            day = date[:date_length - 7]
+        elif date_length == 10:
+            day = date[:date_length - 8]
 
     elif date[:date_length - 7].find('.') != 0:
         day = date[date_length - 2:]
